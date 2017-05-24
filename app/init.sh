f55746b496d8967d5ac3f49dac7cf7a03ff652d0
Ricardo Bravo <rbravo@datiobd.com>
2017-05-24 15:36:58 +0200

    Update init.sh

--- app/init.sh
+++ app/init.sh
@@ -7,5 +7,6 @@ virtualenv -p /usr/bin/python$VERSION /venv
 source /venv/bin/activate
 # install python dependencies into venv
 pip install -r /app/requirements.txt --upgrade
+pip install /app/lib/pymesos-0.2.13.tar.gz
 # run our app inside the container
 /venv/bin/python$VERSION /app/app.py $MASTER_IP
