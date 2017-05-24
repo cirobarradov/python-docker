ce3e77e98f831d79c337dd116a22224da2c77de2
Ricardo Bravo <rbravo@datiobd.com>
2017-05-24 10:31:29 +0200

    Add files via upload

--- app/init.sh
+++ app/init.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # create a virtualenv we can later use
-#mkdir -p /venv/ 
 # install python version on virtual environment
-virtualenv -p /usr/bin/python$VERSION venv 
+virtualenv -p /usr/bin/python$VERSION venv
 #activate virtual environment
-source venv/bin/activate 
+source venv/bin/activate
 # install python dependencies into venv
-pip install -r /app/requirements.txt --upgrade 
+pip install -r /app/requirements.txt --upgrade
 # run our app inside the container
 venv/bin/python$VERSION /app/app.py
-
